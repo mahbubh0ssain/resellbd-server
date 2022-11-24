c6e6f56120702e001ef3ac2759f308261d5765cb
Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2022-11-24 11:20:54 +0600

    db connected

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ app.get("/", (req, res) => {
   res.send("Final assignment server is successfully");
 });
 
+//mongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@resell-bd.6rcesuv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log("DB connected successfully.");
+  } catch (err) {
+    console.error(err);
+  }
+};
+dbConnect();
+
+// userCollection
+const UsersCollection = client.db("Resell-BD").collection("usersCollection");
+
+
+
+
 app.listen(port, () => {
   console.log(`Final assignment server is running on port ${port}`);
 });
